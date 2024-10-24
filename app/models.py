7320a6342fe6adbdc50193e8c5f81573b925f6cf
Daniel Spiers <danjspiers@gmail.com>
2024-10-24 17:54:26 +0100

    Database model added - Initial migration committed

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
 from app import db
+
+class Assessment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    module = db.Column(db.String(10), nullable=False)
+    deadline = db.Column(db.DateTime)
+    description = db.Column(db.String(500))
+    completed = db.Column(db.Boolean)
