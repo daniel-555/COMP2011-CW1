d09a31424276adb355389f05cd38dc75090e2c36
Daniel Spiers <danjspiers@gmail.com>
2024-10-25 16:12:57 +0100

    cards now dictionary-based

--- app/views.py
+++ app/views.py
@@ -5,31 +5,34 @@ from app import app
 def home():
     # Assessments are fetched from database
     # Then processed and passed to template
-    #   in the format {title, module, description, dueDate, dueTime, completed}
+    #   in the format seen below
     
     assessments = [
         { 
+            "id": 1,
             "title":"Coursework 1", 
             "module":"COMP2011", 
             "description":"web app dev coursework 1", 
-            "date":"31/10/24", 
-            "time":"15:00", 
+            "dueDate":"31/10/24", 
+            "dueTime":"15:00", 
             "completed": False
         }, {
+            "id": 2,
             "title":"Coursework 2", 
             "module":"COMP1921", 
             "description":"module from last year's coursework", 
-            "date":"15/2/24", 
-            "time":"14:00", 
+            "dueDate":"15/2/24", 
+            "dueTime":"14:00", 
             "completed": True
         }, {
+            "id": 3,
             "title":"Formative Coursework 1", 
             "module":"COMP2421", 
             "description":"Made up coursework", 
-            "date":"25/10/24", 
-            "time":"16:00", 
+            "dueDate":"25/10/24", 
+            "dueTime":"16:00", 
             "completed": False
         }
     ]
     
-    return render_template("home.html", assessments = assessments)
+    return render_template("home.html", assessments=assessments)
