0a442cb7dcb791746a1f9744a4c3a877f49a8196
Daniel Spiers <danjspiers@gmail.com>
2024-10-27 15:08:45 +0000

    general tidying - made assessments examples global in views

--- app/views.py
+++ app/views.py
@@ -1,48 +1,51 @@
 from flask import render_template
-from app import app
+from app import app, db, models
 from .forms import AssessmentForm
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    # Assessments are fetched from database
+# Assessments are fetched from database
     # Then processed and passed to template
     #   in the format seen below
     
-    assessments = [
-        { 
-            "id": 1,
-            "title":"Coursework 1", 
-            "module":"COMP2011", 
-            "description":"web app dev coursework 1", 
-            "dueDate":"31/10/24", 
-            "dueTime":"15:00", 
-            "completed": False
-        }, {
-            "id": 2,
-            "title":"Coursework 2", 
-            "module":"COMP1921", 
-            "description":"module from last year's coursework", 
-            "dueDate":"15/2/24", 
-            "dueTime":"14:00", 
-            "completed": True
-        }, {
-            "id": 3,
-            "title":"Formative Coursework 1", 
-            "module":"COMP2421", 
-            "description":"Made up coursework", 
-            "dueDate":"25/10/24", 
-            "dueTime":"16:00", 
-            "completed": False
-        }, {
-            "id": 4,
-            "title":"Formative Coursework 1", 
-            "module":"COMP2421", 
-            "description":"Made up coursework", 
-            "dueDate":"25/10/24", 
-            "dueTime":"16:00", 
-            "completed": True
-        }
-    ]
+assessments = [
+    { 
+        "id": 1,
+        "title":"Coursework 1", 
+        "module":"COMP2011", 
+        "description":"web app dev coursework 1", 
+        "dueDate":"31/10/24", 
+        "dueTime":"15:00", 
+        "completed": False
+    }, {
+        "id": 2,
+        "title":"Coursework 2", 
+        "module":"COMP1921", 
+        "description":"module from last year's coursework", 
+        "dueDate":"15/2/24", 
+        "dueTime":"14:00", 
+        "completed": True
+    }, {
+        "id": 3,
+        "title":"Formative Coursework 1", 
+        "module":"COMP2421", 
+        "description":"Made up coursework", 
+        "dueDate":"25/10/24", 
+        "dueTime":"16:00", 
+        "completed": False
+    }, {
+        "id": 4,
+        "title":"Formative Coursework 1", 
+        "module":"COMP2421", 
+        "description":"Made up coursework", 
+        "dueDate":"25/10/24", 
+        "dueTime":"16:00", 
+        "completed": True
+    }
+]
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    
     
     return render_template("home.html", title="Home", assessments=assessments)
 
