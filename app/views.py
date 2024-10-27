b2c6cff774c20810efc923eb9abbbcdb3860ffd1
Daniel Spiers <danjspiers@gmail.com>
2024-10-27 17:32:52 +0000

    - All links functional - Display only complete/current assessments function added - Message displayed when no assessments to display

--- app/views.py
+++ app/views.py
@@ -6,46 +6,30 @@ from .forms import AssessmentForm
 import datetime
 
 # Assessments are fetched from database
-    # Then processed and passed to template
-    #   in the format seen below
-    
-assessments = [
-    { 
-        "id": 0,
-        "title":"Coursework 1", 
-        "module":"COMP2011", 
-        "description":"web app dev coursework 1", 
-        "dueDate":"31/10/24", 
-        "dueTime":"15:00", 
-        "completed": False
-    }, {
-        "id": 1,
-        "title":"Coursework 2", 
-        "module":"COMP1921", 
-        "description":"module from last year's coursework", 
-        "dueDate":"15/2/24", 
-        "dueTime":"14:00", 
-        "completed": True
-    }, {
-        "id": 2,
-        "title":"Formative Coursework 1", 
-        "module":"COMP2421", 
-        "description":"Made up coursework", 
-        "dueDate":"25/10/24", 
-        "dueTime":"16:00", 
-        "completed": False
-    }, {
-        "id": 3,
-        "title":"Formative Coursework 1", 
-        "module":"COMP2421", 
-        "description":"Made up coursework", 
-        "dueDate":"25/10/24", 
-        "dueTime":"16:00", 
-        "completed": True
-    }
-]
+# Then formatted and passed to template
+#   the below array is an example of a the renderable format
+# 
+# assessments = [
+#     { 
+#         "id": 0,
+#         "title":"Coursework 1", 
+#         "module":"COMP2011", 
+#         "description":"web app dev coursework 1", 
+#         "dueDate":"31/10/24", 
+#         "dueTime":"15:00", 
+#         "completed": False
+#     }, {
+#         "id": 1,
+#         "title":"Coursework 2", 
+#         "module":"COMP1921", 
+#         "description":"module from last year's coursework", 
+#         "dueDate":"15/2/24", 
+#         "dueTime":"14:00", 
+#         "completed": True
+#     }
+# ]
 
-
+# Convert a database assessment to the renderable format
 def formatAssessment(assessment):
     return {
             "id": assessment.id,
@@ -57,22 +41,46 @@ def formatAssessment(assessment):
             "completed": assessment.completed
         }
 
-def getAllAssessments():
-    data = db.session.execute(db.select(Assessment).order_by(Assessment.completed)).all()
-    
-    # format data into the usable format
-    formattedAssessments = []
-    for assessment in data:
-        formattedAssessments.append(formatAssessment(assessment[0]))
-
-    return formattedAssessments
-
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    assessments = getAllAssessments()
+    # Get all assessments from database ordered by not completed first
+    data = db.session.execute(db.select(Assessment).order_by(Assessment.completed)).all()
+
+    # Format data ready for rendering
+    assessments = []
+    for assessment in data:
+        assessments.append(formatAssessment(assessment[0]))
+
+    if not assessments:
+        flash("No assessments to display")
 
     return render_template("home.html", title="Home", assessments=assessments)
+
+@app.route('/completed', methods=['GET', 'POST'])
+def completed():
+    data = db.session.execute(db.select(Assessment).filter_by(completed=True)).all()
+
+    assessments = []
+    for assessment in data:
+        assessments.append(formatAssessment(assessment[0]))
+
+    if not assessments:
+        flash("No completed assessments to display")
+
+    return render_template("home.html", title="Completed Assessments", assessments=assessments)
+
+@app.route('/current', methods=['GET', 'POST'])
+def current():
+    data = db.session.execute(db.select(Assessment).filter_by(completed=False)).all()
+
+    assessments = []
+    for assessment in data:
+        assessments.append(formatAssessment(assessment[0]))
+
+    if not assessments:
+        flash("No current assessments to display")
+
+    return render_template("home.html", title="Current Assessments", assessments=assessments)
 
 @app.route('/create', methods=['GET', 'POST'])
 def createAssessment():
@@ -93,7 +101,7 @@ def createAssessment():
         db.session.add(newAssessment)
         db.session.commit()
 
-        return redirect("/")
+        return redirect("/current")
 
     return render_template("assessmentForm.html", 
                            title="Create Assessment", 
@@ -115,7 +123,6 @@ def editAssessment(assessmentId=None):
     return render_template("assessmentForm.html", 
                            title="Edit Assessment", 
                            action="edit", # can be (create, edit)
-                           assessment=assessments[assessmentId],
                            form=form
                            )
 
